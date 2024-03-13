50696aa36be0c258f6a4dc01af2589c9b8be2843
Uche Obiekwe <obiekweuchenna@gmail.com>
2024-03-13 14:59:53 +0100

    Adding mock data

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import { readFileSync } from "fs";
-import path from "path";
 import { gql } from "graphql-tag";
+import path from "path";
 
 const typeDefs = gql(
   readFileSync(path.resolve(__dirname, "./schema.graphql"), {
@@ -10,8 +12,25 @@ const typeDefs = gql(
   })
 );
 
+const mocks = {
+  Query: () => ({
+    featuredPlaylists: () => [...new Array(6)],
+  }),
+  Playlist: () => ({
+    id: () => "playlist_01",
+    name: () => "Groovin' with GraphQL",
+    description: () =>
+      "Serving up the hottest development hits, Groovin' with GraphQL has everything you need to get into the coding mindspace... and stay there!",
+  }),
+};
+
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs });
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
+  });
   const { url } = await startStandaloneServer(server);
   console.log(`
     🚀  Server is running!
